2aff992f97bc97cb70b44ac3e4727cd8bb45ad65
Egor <egor.rajewski@gmail.com>
2019-12-06 22:53:15 +0200

    first landing

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var defaults = {
+        containerID: 'toTop', // fading element id
+        containerHoverID: 'toTopHover', // fading element hover id
+        scrollSpeed: 1200,
+        easingType: 'linear'
+    };
+    $().UItoTop({ easingType: 'easeOutQuart' });
+
+});
+
+$(document).ready(function () {
+    $("a[href^='#']").click(function () {
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination
+        }, 1200);
+        return false;
+    });
+});
